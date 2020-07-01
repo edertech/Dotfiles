36db9ae7d2a931582011c83f64d908afac42abaf
Ederson <dj.edertech.develop@gmail.com>
2020-06-30 22:18:34 -0300

    updated xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -41,6 +41,8 @@ import qualified Data.ByteString as B
 
 import Control.Monad (liftM2)
 
+import XMonad.Layout.ResizableTile
+
 
 ----------------------
 -- VARIABLES
@@ -87,7 +89,7 @@ myLayoutHook = spacingRaw True (Border 0 5 5 5) True (Border 5 5 5 5) True $ gap
                $ avoidStruts
                $ mkToggle (NBFULL ?? NOBORDERS ?? EOT)
                $ smartBorders
-               $ tiled ||| Grid ||| spiral (6/7) ||| ThreeColMid 1 (3/100) (1/2) ||| noBorders Full
+               $ ResizableTall 1 (3/100) (1/2) [] ||| tiled ||| Grid ||| spiral (6/7) ||| ThreeColMid 1 (3/100) (1/2) ||| noBorders Full
                     where
                     tiled   = Tall nmaster delta ratio
                     nmaster = 1
@@ -113,16 +115,6 @@ myManageHook = composeAll . concat $
     , [title =? t --> doFloat | t <- myTFloats]
     , [resource =? r --> doFloat | r <- myRFloats]
     , [resource =? i --> doIgnore | i <- myIgnores]
---    , [className =? c --> doShift (myWorkspaces !! 0) <+> viewShift (myWorkspaces !! 0)        | c <- my1Shifts]
---    , [className =? c --> doShift (myWorkspaces !! 1) <+> viewShift (myWorkspaces !! 1)        | c <- my2Shifts]
---    , [className =? c --> doShift (myWorkspaces !! 2) <+> viewShift (myWorkspaces !! 2)        | c <- my3Shifts]
---    , [className =? c --> doShift (myWorkspaces !! 3) <+> viewShift (myWorkspaces !! 3)        | c <- my4Shifts]
---    , [className =? c --> doShift (myWorkspaces !! 4) <+> viewShift (myWorkspaces !! 4)        | c <- my5Shifts]
---    , [className =? c --> doShift (myWorkspaces !! 5) <+> viewShift (myWorkspaces !! 5)        | c <- my6Shifts]
---    , [className =? c --> doShift (myWorkspaces !! 6) <+> viewShift (myWorkspaces !! 6)        | c <- my7Shifts]
---    , [className =? c --> doShift (myWorkspaces !! 7) <+> viewShift (myWorkspaces !! 7)        | c <- my8Shifts]
---    , [className =? c --> doShift (myWorkspaces !! 8) <+> viewShift (myWorkspaces !! 8)        | c <- my9Shifts]
---    , [className =? c --> doShift (myWorkspaces !! 9) <+> viewShift (myWorkspaces !! 9)        | c <- my10Shifts]
        ]
     where
 --    viewShift    = doF . liftM2 (.) W.greedyView W.shift
@@ -130,16 +122,6 @@ myManageHook = composeAll . concat $
     myTFloats = ["Downloads", "Save As..."]
     myRFloats = []
     myIgnores = ["desktop_window"]
-    -- my1Shifts = ["Chromium", "Vivaldi-stable", "Firefox"]
-    -- my2Shifts = []
-    -- my3Shifts = ["Inkscape"]
-    -- my4Shifts = []
-    -- my5Shifts = ["Gimp", "feh"]
-    -- my6Shifts = ["vlc", "mpv"]
-    -- my7Shifts = ["Virtualbox"]
-    -- my8Shifts = ["Thunar"]
-    -- my9Shifts = []
-    -- my10Shifts = ["discord"]
 
 -- keys config
 
@@ -149,6 +131,8 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
 
   [ ((modMask, xK_a), spawn $ "xfce4-appfinder" )
   , ((modMask, xK_b), spawn $ "meld" )
+
+
   , ((modMask, xK_c), spawn $ "conky-toggle" )
   --, ((modMask, xK_d), spawn $ "rofi -show run" )
   , ((modMask, xK_e), spawn $ "emacs" )
@@ -161,24 +145,13 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
   , ((modMask, xK_r), spawn $ "rofi -show run")
   , ((modMask, xK_s), spawn $ "spotify")
   , ((modMask, xK_t), spawn $ "urxvt" )
+  , ((modMask, xK_u), sendMessage MirrorExpand)
   , ((modMask, xK_v), spawn $ "pavucontrol" )
   , ((modMask, xK_w), spawn $ "qutebrowser" )
   , ((modMask, xK_y), spawn $ "polybar-msg cmd toggle" )
   , ((modMask, xK_x), spawn $ "arcolinux-logout" )
   , ((modMask, xK_Escape), spawn $ "xkill" )
   , ((modMask, xK_Return), spawn $ myTerminal ++ " -e fish" )
-  -- , ((modMask, xK_F1), spawn $ "vivaldi-stable" )
-  -- , ((modMask, xK_F2), spawn $ "atom" )
-  -- , ((modMask, xK_F3), spawn $ "inkscape" )
-  -- , ((modMask, xK_F4), spawn $ "gimp" )
-  -- , ((modMask, xK_F5), spawn $ "meld" )
-  -- , ((modMask, xK_F6), spawn $ "vlc --video-on-top" )
-  -- , ((modMask, xK_F7), spawn $ "virtualbox" )
-  -- , ((modMask, xK_F8), spawn $ "thunar" )
-  -- , ((modMask, xK_F9), spawn $ "evolution" )
-  -- , ((modMask, xK_F10), spawn $ "spotify" )
-  -- , ((modMask, xK_F11), spawn $ "rofi -show run -fullscreen" )
-  -- , ((modMask, xK_F12), spawn $ "rofi -show run" )
 
   -- FUNCTION KEYS
   , ((0, xK_F12), spawn $ "xfce4-terminal --drop-down" )
@@ -190,53 +163,6 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
   , ((modMask .|. shiftMask , xK_r ), spawn $ "xmonad --recompile && xmonad --restart")
   , ((modMask .|. shiftMask , xK_q ), kill)
   , ((modMask .|. shiftMask , xK_x ), io (exitWith ExitSuccess))
-
-  -- CONTROL + ALT KEYS
-
-  -- , ((controlMask .|. mod1Mask , xK_Next ), spawn $ "conky-rotate -n")
-  -- , ((controlMask .|. mod1Mask , xK_Prior ), spawn $ "conky-rotate -p")
-  -- , ((controlMask .|. mod1Mask , xK_a ), spawn $ "xfce4-appfinder")
-  -- , ((controlMask .|. mod1Mask , xK_b ), spawn $ "thunar")
-  -- , ((controlMask .|. mod1Mask , xK_c ), spawn $ "catfish")
-  -- , ((controlMask .|. mod1Mask , xK_e ), spawn $ "arcolinux-tweak-tool")
-  -- , ((controlMask .|. mod1Mask , xK_f ), spawn $ "firefox")
-  -- , ((controlMask .|. mod1Mask , xK_g ), spawn $ "chromium -no-default-browser-check")
-  -- , ((controlMask .|. mod1Mask , xK_i ), spawn $ "nitrogen")
-  -- , ((controlMask .|. mod1Mask , xK_k ), spawn $ "arcolinux-logout")
-  -- , ((controlMask .|. mod1Mask , xK_l ), spawn $ "arcolinux-logout")
-  -- , ((controlMask .|. mod1Mask , xK_m ), spawn $ "xfce4-settings-manager")
-  -- , ((controlMask .|. mod1Mask , xK_o ), spawn $ "$HOME/.xmonad/scripts/picom-toggle.sh")
-  -- , ((controlMask .|. mod1Mask , xK_p ), spawn $ "pamac-manager")
-  -- , ((controlMask .|. mod1Mask , xK_r ), spawn $ "rofi-theme-selector")
-  -- , ((controlMask .|. mod1Mask , xK_s ), spawn $ "spotify")
-  -- , ((controlMask .|. mod1Mask , xK_t ), spawn $ "urxvt")
-  -- , ((controlMask .|. mod1Mask , xK_u ), spawn $ "pavucontrol")
-  -- , ((controlMask .|. mod1Mask , xK_v ), spawn $ "vivaldi-stable")
-  -- , ((controlMask .|. mod1Mask , xK_w ), spawn $ "arcolinux-welcome-app")
-  -- , ((controlMask .|. mod1Mask , xK_Return ), spawn $ "urxvt")
-
-  -- ALT + ... KEYS
-
-  -- , ((mod1Mask, xK_f), spawn $ "variety -f" )
-  -- , ((mod1Mask, xK_n), spawn $ "variety -n" )
-  -- , ((mod1Mask, xK_p), spawn $ "variety -p" )
-  -- , ((mod1Mask, xK_r), spawn $ "xmonad --restart" )
-  -- , ((mod1Mask, xK_t), spawn $ "variety -t" )
-  -- , ((mod1Mask, xK_Up), spawn $ "variety --pause" )
-  -- , ((mod1Mask, xK_Down), spawn $ "variety --resume" )
-  -- , ((mod1Mask, xK_Left), spawn $ "variety -p" )
-  -- , ((mod1Mask, xK_Right), spawn $ "variety -n" )
-  -- , ((mod1Mask, xK_F2), spawn $ "gmrun" )
-  -- , ((mod1Mask, xK_F3), spawn $ "xfce4-appfinder" )
-
-  --VARIETY KEYS WITH PYWAL
-
-  -- , ((mod1Mask .|. shiftMask , xK_f ), spawn $ "variety -f && wal -i $(cat $HOME/.config/variety/wallpaper/wallpaper.jpg.txt)&")
-  -- , ((mod1Mask .|. shiftMask , xK_n ), spawn $ "variety -n && wal -i $(cat $HOME/.config/variety/wallpaper/wallpaper.jpg.txt)&")
-  -- , ((mod1Mask .|. shiftMask , xK_p ), spawn $ "variety -p && wal -i $(cat $HOME/.config/variety/wallpaper/wallpaper.jpg.txt)&")
-  -- , ((mod1Mask .|. shiftMask , xK_t ), spawn $ "variety -t && wal -i $(cat $HOME/.config/variety/wallpaper/wallpaper.jpg.txt)&")
-  -- , ((mod1Mask .|. shiftMask , xK_u ), spawn $ "wal -i $(cat $HOME/.config/variety/wallpaper/wallpaper.jpg.txt)&")
-
   , ((0, xK_Print), spawn $ "scrot 'ArcoLinux-%Y-%m-%d-%s_screenshot_$wx$h.jpg' -e 'mv $f $$(xdg-user-dir PICTURES)'")
   , ((controlMask, xK_Print), spawn $ "xfce4-screenshooter" )
   , ((controlMask .|. shiftMask , xK_Print ), spawn $ "gnome-screenshot -i")
@@ -258,11 +184,6 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
 
   -- Decrease brightness
   , ((0, xF86XK_MonBrightnessDown), spawn $ "xbacklight -dec 5")
-
---  , ((0, xF86XK_AudioPlay), spawn $ "mpc toggle")
---  , ((0, xF86XK_AudioNext), spawn $ "mpc next")
---  , ((0, xF86XK_AudioPrev), spawn $ "mpc prev")
---  , ((0, xF86XK_AudioStop), spawn $ "mpc stop")
 
   , ((0, xF86XK_AudioPlay), spawn $ "playerctl play-pause")
   , ((0, xF86XK_AudioNext), spawn $ "playerctl next")
@@ -318,6 +239,9 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
   -- Expand the master area.
   , ((controlMask .|. shiftMask , xK_l), sendMessage Expand)
 
+  --
+  -- Resizable Tile
+  --
   -- Push window back into tiling.
   , ((controlMask .|. shiftMask , xK_t), withFocused $ windows . W.sink)
 
@@ -337,12 +261,6 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
   --Keyboard layouts
   --qwerty users use this line
    | (i, k) <- zip (XMonad.workspaces conf) [xK_1,xK_2,xK_3,xK_4,xK_5,xK_6,xK_7,xK_8,xK_9,xK_0]
-
-  --French Azerty users use this line
-  -- | (i, k) <- zip (XMonad.workspaces conf) [xK_ampersand, xK_eacute, xK_quotedbl, xK_apostrophe, xK_parenleft, xK_minus, xK_egrave, xK_underscore, xK_ccedilla , xK_agrave]
-
-  --Belgian Azerty users use this line
-  --   | (i, k) <- zip (XMonad.workspaces conf) [xK_ampersand, xK_eacute, xK_quotedbl, xK_apostrophe, xK_parenleft, xK_section, xK_egrave, xK_exclam, xK_ccedilla, xK_agrave]
 
       , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)
       , (\i -> W.greedyView i . W.shift i, shiftMask)]]
