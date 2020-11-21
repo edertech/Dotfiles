c93cd6281e654ba830f808d8f9a07fc0f996be3f
Ederson <dj.edertech.develop@gmail.com>
2020-11-21 01:31:07 -0300

    fish config updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
-set PATH $PATH /home/ederson/.local/bin /home/ederson/.gem/ruby/2.7.0/bin
+set PATH $PATH /home/ederson/.local/bin /home/ederson/.gem/ruby/2.7.0/bin ~/.emacs.d/bin
 
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 
 alias vim=nvim
+
+set EDITOR vim
