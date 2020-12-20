16bfbd4453b76f59568a22c0f02bd4c8a0c570d0
Ederson <dj.edertech.develop@gmail.com>
2020-12-20 20:13:23 -0300

    config updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,9 @@ set PATH $PATH /home/ederson/.local/bin /home/ederson/.gem/ruby/2.7.0/bin ~/.ema
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 
 alias vim=nvim
+alias upall='yay -Syu --noconfirm'
+alias pacman='sudo pacman --color auto'
 
 set EDITOR vim
+
+neofetch
