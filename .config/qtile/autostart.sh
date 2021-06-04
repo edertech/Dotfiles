f2c0c7612a845b464a4365260923765ba022a950
Ederson <dj.edertech.develop@gmail.com>
2021-06-04 12:33:09 -0300

    Added my personal Qtile config

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash 
+
+festival --tts $HOME/.config/qtile/welcome_msg &
+lxsession &
+picom &
+nitrogen --restore &
+/usr/bin/emacs --daemon &
+volumeicon &
+nm-applet &
+# replace capslock with control key
+xmodmap ~/.Xmodmap
